c33ee7ec85d0bee080118deee170153536b03d16
Maicon Douglas <mdprogamacao@outlook.com>
2025-05-12 23:51:11 -0300

    criando pagina de login do usario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginUserComponentComponent } from './login-user-component/login-user-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginUserComponentComponent,
+    }
+];
---
+++ src/app/login-user-component/login-user-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-user-component',
+  imports: [],
+  templateUrl: './login-user-component.component.html',
+  styleUrl: './login-user-component.component.css'
+})
+export class LoginUserComponentComponent {
+
+}
