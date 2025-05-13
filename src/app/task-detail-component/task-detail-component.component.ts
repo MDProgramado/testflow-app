e5c393bc4303e49f79ed28b33204c7c4a05e99c2
Maicon Douglas <mdprogamacao@outlook.com>
2025-05-12 23:27:16 -0300

    resolvendo estruturação do projeto, criação dos principas compoentes do projeto, instalação do boostrap e configurando

---
+++ src/app/task-detail-component/task-detail-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-detail-component',
+  imports: [],
+  templateUrl: './task-detail-component.component.html',
+  styleUrl: './task-detail-component.component.css'
+})
+export class TaskDetailComponentComponent {
+
+}
---
+++ src/app/task-form-component/task-form-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form-component',
+  imports: [],
+  templateUrl: './task-form-component.component.html',
+  styleUrl: './task-form-component.component.css'
+})
+export class TaskFormComponentComponent {
+
+}
